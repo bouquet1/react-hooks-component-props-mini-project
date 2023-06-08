902f6bca75629b614bd3944e00be9f73a0cad91a
bouquet1 <bukuleta@gmail.com>
2023-06-08 13:33:09 -0700

    Article comp refactored   for npm test

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,5 +1,22 @@
 import React from "react";
 
+export default function Article({
+  title = "Components 101",
+  date = "January 1, 1970",
+  preview = "Setting up the building blocks of your site",
+  minutes,
+}) {
+  return (
+    <article>
+      <h3>{title}</h3>
+      <small>{date}</small>
+      <p>{preview}</p>
+    </article>
+  );
+}
+
+//boyle yaptim test den omce calisiyor gayet guzel ama illa testin istedigi sekil yaptim yukarda
+/*
 export default function Article({ post }) {
   const { title, date = "January 1, 1970", preview, minutes } = post;
   return (
@@ -10,3 +27,4 @@ export default function Article({ post }) {
     </article>
   );
 }
+*/
